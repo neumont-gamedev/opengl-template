5dbc5ca55029eca3ab008953a289a15edbf0ad00
NEUMONT\rmaple <rmaple@neumont.edu>
2017-10-03 17:17:30 -0600

    update commit

---
+++ Engine/engine.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+
+#include <stdlib.h>
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+int main()
+{
+	// initialize opengl/window
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "computer graphics", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwMakeContextCurrent(window);
+
+	// render code
+
+	// main game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+    return 0;
+}
+
